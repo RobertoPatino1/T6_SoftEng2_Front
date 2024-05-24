b9279adec6d8cf494a4f362dff0a9b9e11676f4c
Roberto Patiño <rapatino@espol.edu.ec>
2024-05-24 16:20:53 -0500

    Initial structure for login page

--- share_your_route_front/lib/main.dart
+++ share_your_route_front/lib/main.dart
@@ -1,21 +1,69 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amberAccent,
-          title: const Text("Share Your Route"),
-        ),
-      ),
-    );
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+        Colors.blue.shade900,
+        Colors.blue.shade800,
+        Colors.blue.shade400
+      ])),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 80,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                    Text(
+                      "Welcome Back",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                  ]),
+            ),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60))),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(children: <Widget>[
+                  Container(
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(color: Colors.grey[200]!))),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Colors.grey),
+                            border: InputBorder.none),
+                      )),
+                ]),
+              ),
+            ))
+          ]),
+    ));
   }
 }
