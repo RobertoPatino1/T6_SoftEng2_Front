5ce1ae4de7e59a5e0526d9a28ec6e343f5e19c3a
gjareval <gjareval@espol.edu.ec>
2024-05-24 22:37:36 -0500

    Posible pantalla de login y home

--- share_your_route_front/lib/main.dart
+++ share_your_route_front/lib/main.dart
@@ -1,69 +1,108 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:share_your_route_front/home.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(),
     ));
 
-class HomePage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-        Colors.blue.shade900,
-        Colors.blue.shade800,
-        Colors.blue.shade400
-      ])),
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(
-              height: 80,
-            ),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "Login",
-                      style: TextStyle(color: Colors.white, fontSize: 40),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 60.0),
+                  child: Center(
+                    child: Container(
+                      width: 200,
+                      height: 150,
+                      child: Image.asset('asset/images/logo.png'),
                     ),
-                    Text(
-                      "Welcome Back",
-                      style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      hintText: 'Email válido como: abc@gmail.com',
                     ),
-                  ]),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 15.0,
+                    right: 15.0,
+                    top: 15,
+                  ),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Contraseña',
+                      hintText: 'Ingrese una contraseña segura',
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    //TODO FORGOT PASSWORD SCREEN GOES HERE
+                  },
+                  child: Text(
+                    'Olvidaste tu contraseña?',
+                    style: TextStyle(
+                      color: Color.fromRGBO(37, 60, 89, 1),
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(37, 60, 89, 1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => HomePage()),
+                      );
+                    },
+                    child: Text(
+                      'Iniciar sesión',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 130),
+                Text('Usuario nuevo? Crea una cuenta'),
+              ],
             ),
-            Expanded(
-                child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(60),
-                      topRight: Radius.circular(60))),
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(children: <Widget>[
-                  Container(
-                      decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(color: Colors.grey[200]!))),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            hintText: "Email",
-                            hintStyle: TextStyle(color: Colors.grey),
-                            border: InputBorder.none),
-                      )),
-                ]),
-              ),
-            ))
-          ]),
-    ));
+          ),
+        ),
+      ),
+    );
   }
 }
