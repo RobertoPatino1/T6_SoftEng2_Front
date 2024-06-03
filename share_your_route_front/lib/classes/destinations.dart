e3a4cd53ce2ccff8e6604c021949daad866daffe
gjareval <gjareval@espol.edu.ec>
2024-06-02 21:01:37 -0500

    Cambios en pantalla home

---
+++ share_your_route_front/lib/classes/destinations.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Destinations {
+  final String name;
+  final double duration;
+  final String description;
+  final String startTime;
+
+  Destinations(
+      {required this.name,
+      required this.duration,
+      required this.description,
+      required this.startTime});
+}
--- share_your_route_front/lib/home.dart
+++ share_your_route_front/lib/home.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:share_your_route_front/classes/destinations.dart';
 
-/// Flutter code sample for [NavigationBar].
+List<Destinations> destinations = [
+  Destinations(
+      name: "Centro artístico",
+      duration: 3,
+      description: "Descubre las maravillas artísticas",
+      startTime: "12:00"),
+  Destinations(
+      name: "Fauna silvestre",
+      duration: 5,
+      description: "Descubre la fauna del lugar",
+      startTime: "10:00"),
+  Destinations(
+      name: "Ruta gastronómica",
+      duration: 6,
+      description: "Encuentra los mejores platos típicos",
+      startTime: "14:00"),
+];
 
 void main() => runApp(const HomePage());
 
@@ -30,13 +47,15 @@ class _NavigationExampleState extends State<NavigationExample> {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return Scaffold(
+      backgroundColor: Color.fromRGBO(211, 225, 244, 1),
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
           setState(() {
             currentPageIndex = index;
           });
         },
-        indicatorColor: Colors.amber,
+        indicatorColor: const Color.fromRGBO(37, 60, 89, 0.5),
+        backgroundColor: const Color.fromRGBO(37, 60, 89, 0.1),
         selectedIndex: currentPageIndex,
         destinations: const <Widget>[
           NavigationDestination(
@@ -45,64 +64,197 @@ class _NavigationExampleState extends State<NavigationExample> {
             label: 'Home',
           ),
           NavigationDestination(
-            icon: Badge(child: Icon(Icons.notifications_sharp)),
+            icon: Icon(Icons.notifications_sharp),
             label: 'Notifications',
           ),
           NavigationDestination(
-            icon: Badge(
-              label: Text('2'),
-              child: Icon(Icons.messenger_sharp),
-            ),
+            icon: Icon(Icons.messenger_sharp),
             label: 'Messages',
           ),
         ],
       ),
-      body: <Widget>[
-        /// Home page
-        Card(
-          shadowColor: Colors.transparent,
-          margin: const EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Home page',
-                style: theme.textTheme.titleLarge,
-              ),
-            ),
-          ),
-        ),
-
-        /// Notifications page
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
+      body: IndexedStack(
+        index: currentPageIndex,
+        children: <Widget>[
+          // Home page
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.notifications_sharp),
-                  title: Text('Notification 1'),
-                  subtitle: Text('This is a notification'),
+              Container(
+                margin:
+                    EdgeInsets.only(top: 50, bottom: 10, left: 10, right: 20),
+                child: Text(
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontSize: 35.0,
+                        color: Color.fromRGBO(45, 75, 115, 1),
+                        fontWeight: FontWeight.bold,
+                        height: 1),
+                    "Empecemos una aventura!"),
+              ),
+              Container(
+                margin: EdgeInsets.all(5),
+                child: Text(
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 15.0,
+                        color: Color.fromRGBO(45, 75, 115, 1),
+                        fontWeight: FontWeight.normal),
+                    "¿Deseas crear una ruta?"),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                height: 40,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(45, 75, 115, 1),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextButton(
+                    onPressed: () {},
+                    child: const Row(
+                      children: [
+                        Text(
+                          'Empezar una ruta',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 2.1,
+                        ),
+                        Icon(
+                          Icons.play_arrow,
+                          color: Colors.white,
+                          size: 15,
+                        ),
+                      ],
+                    )),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis
+                      .horizontal, // Establece la dirección de desplazamiento horizontal
+                  itemCount: destinations.length,
+                  itemBuilder: (context, index) {
+                    Destinations destination =
+                        destinations[index]; // Obtener el destino actual
+
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Row(
+                        // Envuelve el Container en un Row
+                        children: [
+                          Container(
+                            width: 160,
+                            color: Colors.blue,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(destination.name,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                Text("Duración: ${destination.duration} horas"),
+                                Text(destination.description),
+                                Text(
+                                    "Hora de inicio: ${destination.startTime}"),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.notifications_sharp),
-                  title: Text('Notification 2'),
-                  subtitle: Text('This is a notification'),
+              SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis
+                      .horizontal, // Establece la dirección de desplazamiento horizontal
+                  itemCount: destinations.length,
+                  itemBuilder: (context, index) {
+                    Destinations destination =
+                        destinations[index]; // Obtener el destino actual
+
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Row(
+                        // Envuelve el Container en un Row
+                        children: [
+                          Container(
+                            width: 160,
+                            color: Colors.blue,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(destination.name,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                Text("Duración: ${destination.duration} horas"),
+                                Text(destination.description),
+                                Text(
+                                    "Hora de inicio: ${destination.startTime}"),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ),
             ],
           ),
-        ),
+          // Notifications page
 
-        /// Messages page
-        ListView.builder(
-          reverse: true,
-          itemCount: 2,
-          itemBuilder: (BuildContext context, int index) {
-            if (index == 0) {
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.notifications_sharp),
+                    title: Text('Notification 1'),
+                    subtitle: Text('This is a notification'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.notifications_sharp),
+                    title: Text('Notification 2'),
+                    subtitle: Text('This is a notification'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Messages page
+          ListView.builder(
+            reverse: true,
+            itemCount: 2,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == 0) {
+                return Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: theme.colorScheme.primary,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Text(
+                      'Hello',
+                      style: theme.textTheme.bodyLarge!
+                          .copyWith(color: theme.colorScheme.onPrimary),
+                    ),
+                  ),
+                );
+              }
               return Align(
-                alignment: Alignment.centerRight,
+                alignment: Alignment.centerLeft,
                 child: Container(
                   margin: const EdgeInsets.all(8.0),
                   padding: const EdgeInsets.all(8.0),
@@ -111,32 +263,16 @@ class _NavigationExampleState extends State<NavigationExample> {
                     borderRadius: BorderRadius.circular(8.0),
                   ),
                   child: Text(
-                    'Hello',
+                    'Hi!',
                     style: theme.textTheme.bodyLarge!
                         .copyWith(color: theme.colorScheme.onPrimary),
                   ),
                 ),
               );
-            }
-            return Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                margin: const EdgeInsets.all(8.0),
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: theme.colorScheme.primary,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  'Hi!',
-                  style: theme.textTheme.bodyLarge!
-                      .copyWith(color: theme.colorScheme.onPrimary),
-                ),
-              ),
-            );
-          },
-        ),
-      ][currentPageIndex],
+            },
+          ),
+        ],
+      ),
     );
   }
 }
